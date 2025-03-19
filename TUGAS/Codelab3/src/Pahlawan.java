aad9edb5983514a282a1a022278e26c1d46e5de4
Erlinrsa <erlinmariska2@gmail.com>
2025-03-20 00:58:56 +0700

    update

---
+++ TUGAS/Codelab3/src/Pahlawan.java
@@ -0,0 +1,12 @@
+class Pahlawan extends KarakterGame {
+    public Pahlawan(String nama, int kesehatan) {
+        super(nama, kesehatan);
+    }
+
+    @Override
+    public void serang(KarakterGame target) {
+        System.out.println(getNama() + " menyerang " + target.getNama() + " menggunakan obital strike!");
+        target.setKesehatan(target.getKesehatan() - 20);
+        System.out.println(target.getNama() + " sekarang memiliki kesehatan " + target.getKesehatan());
+    }
+}
